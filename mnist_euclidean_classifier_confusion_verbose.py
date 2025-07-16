f89b2cf92326cee17c101735695295505c6adf7a
austinbenedicto <austinbenedicto271@gmail.com>
2025-07-16 17:37:19 -0400

    commiting my stuff

--- mnist_euclidean_classifier_confusion_verbose.py
+++ mnist_euclidean_classifier_confusion_verbose.py
@@ -13,25 +13,27 @@ LIMIT_PER_DIGIT = 5000  # Limit number of samples per digit
 
 # Define weights for each feature
 FEATURE_WEIGHTS = {
-    'dark_pixel_count': 1.0,
-    'avg_x': 1.0,
-    'avg_y': 1.0,
-    'bbox_height': 1.0,
-    'bbox_width': 1.0,
-    'intersection_count': 1.0,
-    'loop_count': 1.0,
-    'corner_count': 1.0,
-    'symmetry_metric': 1.0,
-    'writing_angle_global': 1.0,
+    'dark_pixel_count': 0.3,
+    'avg_x': 0.5,
+    'avg_y': 0.5,
+    'bbox_height': 0.9,
+    'bbox_width': 0.9,
+    'intersection_count': 1.3,
+    'loop_count': 1.7,
+    'corner_count': 1.6,
+    'symmetry_metric': 1.4,
+
+    'writing_angle_global': 1.5,
     'writing_angle_tl': 1.0,
     'writing_angle_tr': 1.0,
     'writing_angle_bl': 1.0,
     'writing_angle_br': 1.0,
-    'writing_magnitude_global': 1.0,
-    'writing_magnitude_tl': 1.0,
-    'writing_magnitude_tr': 1.0,
-    'writing_magnitude_bl': 1.0,
-    'writing_magnitude_br': 1.0
+
+    'writing_magnitude_global': 1.3,
+    'writing_magnitude_tl': 0.9,
+    'writing_magnitude_tr': 0.9,
+    'writing_magnitude_bl': 0.9,
+    'writing_magnitude_br': 0.9
 }
 
 # Load CSV
